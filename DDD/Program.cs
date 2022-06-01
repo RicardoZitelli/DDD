e775ccc843cf526c13ef4e9c3e9872d54a1fb9ce
Ricardo Zitelli de Oliveira <ricardo.zitelli@compasso.com.br>
2022-06-01 14:15:09 -0300

    Adicionado injeção de dependência

--- DDD/Program.cs
+++ DDD/Program.cs
@@ -1,13 +1,25 @@
+using DDD.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSqlServer<SqlContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+SqlContext dbcontext = app.Services.GetRequiredService<SqlContext>();
+dbcontext.Database.EnsureCreated();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
