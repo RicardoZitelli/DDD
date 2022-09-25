b475061a580e3101771dfb6e01bd910b497f20fc
Ricardo Zitelli de Oliveira <ricardo.zitelli@compasso.com.br>
2022-09-25 17:03:02 -0300

    Alterado tipo da propriedade SqlContext

--- DDD.Infraestrutura/Data/Repositories/RepositoryProduto.cs
+++ DDD.Infraestrutura/Data/Repositories/RepositoryProduto.cs
@@ -5,7 +5,7 @@ namespace DDD.Infrastructure.Data.Repositories
 {
     public class RepositoryProduto : RepositoryBase<Produto>, IRepositoryProduto
     {
-        public readonly SqlContext sqlContext;
+        private readonly SqlContext sqlContext;
 
         public RepositoryProduto(SqlContext sqlContext) : base(sqlContext)  
         {
