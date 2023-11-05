8f7eedfba9549760d37a026034d21ef7397b9620
Ricardo Zitelli <CX-Ricardo.Zitelli@glhomes.com>
2023-11-05 16:55:48 -0300

    Working with docker-compose

--- DDD/Program.cs
+++ DDD/Program.cs
@@ -15,8 +15,15 @@ builder.Services.AddEndpointsApiExplorer();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder
+    .Configuration
+    .GetConnectionString("DefaultConnection")
+    .Replace("{Server}", Environment.GetEnvironmentVariable("DB_HOST"))
+    .Replace("{Database}", "DB_NAME")    
+    .Replace("{Password}", "DB_SA_PASSWORD");
+
 builder.Services.AddDbContext<SqlContext>(options => 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer());
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
