9a64752f672a7564f9c7c4ee092a652988073ef0
Ricardo Zitelli <CX-Ricardo.Zitelli@glhomes.com>
2023-11-05 17:07:55 -0300

    Minor fixes

--- DDD.Dominio/Services/ServiceProduto.cs
+++ DDD.Dominio/Services/ServiceProduto.cs
@@ -5,11 +5,10 @@ using DDD.Domain.Entities;
 namespace DDD.Domain.Services
 {
     public class ServiceProduto : ServiceBase<Produto>, IServiceProduto
-    {
-        private readonly IRepositoryProduto repositoryProduto;
+    {        
         public ServiceProduto(IRepositoryProduto repositoryProduto) : base(repositoryProduto)
         {
-            this.repositoryProduto = repositoryProduto;
+            
         }
     }
 }
--- DDD.Services/Dtos/Requests/TipoProdutoDto.cs
+++ DDD.Services/Dtos/Requests/TipoProdutoDto.cs
@@ -3,7 +3,7 @@
     public class TipoProdutoDto
     {
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string? Descricao { get; set; }
         public DateTime DataCriacao { get; set; }
         public DateTime DataAlteracao { get; set; }
         public bool Ativo { get; set; }
